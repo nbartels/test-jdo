deae171f114837a7c3d2cefc1418a55bcb53771a
Norbert Bartels <n.bartels@phpmonkeys.de>
2015-01-14 13:00:58 +0100

    Bug 5 - max value strategy not working correctly
    If you add two objects with the "max" value strategy in one transaction
    you only get an exception, because the unique key constraint is
    violated. Both objects have the same pkey ...

--- src/main/java/mydomain/model/Person.java
+++ src/main/java/mydomain/model/Person.java
@@ -2,27 +2,19 @@ package mydomain.model;
 
 import javax.jdo.annotations.*;
 
-@PersistenceCapable(detachable="true")
+@PersistenceCapable(detachable="true", identityType = IdentityType.DATASTORE)
+@DatastoreIdentity(customStrategy = "max")
 public class Person
 {
-    @PrimaryKey
-    Long id;
-
     String name;
 
-    public Person(long id, String name)
+    public Person(String name)
     {
-        this.id = id;
         this.name = name;
     }
 
     public String getName()
     {
         return name;
-    }
-
-    public Long getId()
-    {
-        return id;
     }
 }
--- src/test/java/org/datanucleus/test/SimpleTest.java
+++ src/test/java/org/datanucleus/test/SimpleTest.java
@@ -10,7 +10,7 @@ import org.datanucleus.util.NucleusLogger;
 public class SimpleTest
 {
     @Test
-    public void testSimple()
+    public void testSimpleCorrect()
     {
         NucleusLogger.GENERAL.info(">> test START");
         PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("MyTest");
@@ -20,8 +20,50 @@ public class SimpleTest
         try
         {
             tx.begin();
+            Person p1 = new Person("p1");
+            pm.makePersistent(p1);
+            System.out.println("Objectid p1: " + pm.getObjectId(p1));
+            tx.commit();
+            tx.begin();
+            Person p2 = new Person("p2");
+            pm.makePersistent(p2);
+            System.out.println("Objectid p2: " + pm.getObjectId(p2));
+            tx.commit();
+        }
+        catch (Throwable thr)
+        {
+            NucleusLogger.GENERAL.error(">> Exception thrown persisting data", thr);
+            fail("Failed to persist data : " + thr.getMessage());
+        }
+        finally 
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+            pm.close();
+        }
 
-            // [INSERT code here to persist object required for testing]
+        pmf.close();
+        NucleusLogger.GENERAL.info(">> test END");
+    }
+    
+    @Test
+    public void testSimpleBuggy()
+    {
+        NucleusLogger.GENERAL.info(">> test START");
+        PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("MyTest");
+
+        PersistenceManager pm = pmf.getPersistenceManager();
+        Transaction tx = pm.currentTransaction();
+        try
+        {
+            tx.begin();
+            Person p1 = new Person("p1");
+            Person p2 = new Person("p2");
+            pm.makePersistentAll(p1, p2);
+            System.out.println("Objectid p1: " + pm.getObjectId(p1));
+            System.out.println("Objectid p2: " + pm.getObjectId(p2));
             tx.commit();
         }
         catch (Throwable thr)
